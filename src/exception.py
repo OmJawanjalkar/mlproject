714b25cd874c58dedd70651d932bbc1a8f8d7ef5
OmJawanjalkar <omjawanjalkar@gmail.com>
2025-08-05 12:57:33 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -29,7 +29,8 @@
 
 
 import sys
-import logging
+from logger import logging
+
 
 
 def error_message_detail(error, error_detail: sys):
@@ -47,5 +48,13 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e, sys)
 
 
